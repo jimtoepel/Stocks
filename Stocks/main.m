a69ea0f937adb4828adc4dd65f284594cfae1a3b
jimtoepel <jim.toepel@gmail.com>
2015-02-23 20:53:59 -0800

    Initial commit

---
+++ Stocks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Jim Toepel on 2/23/15.
+//  Copyright (c) 2015 FunderDevelopment. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
